6f5443c2397c7d3591038a7fe092968b5a566604
unezb <unezbat@gmail.com>
2026-05-07 18:01:41 +0530

    Adding prometheus stack

--- terraform/3-sg-.tf
+++ terraform/3-sg-.tf
@@ -20,10 +20,35 @@ resource "aws_security_group" "app_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "Grafana"
+    from_port = 3000
+    to_port   = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Prometheus"
+    from_port = 9090
+    to_port   = 9090
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Node Exporter"
+    from_port = 9100
+    to_port   = 9100
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-app-sg"
   }
 }
